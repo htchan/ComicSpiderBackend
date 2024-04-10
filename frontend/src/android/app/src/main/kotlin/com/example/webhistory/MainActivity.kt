4a5bb0514c652792b50296530962f3f0c91c5002
htchan <htchanbc@connect.ust.hk>
2024-04-10 19:23:15 +0800

    chore: remove frontend directory

--- frontend/src/android/app/src/main/kotlin/com/example/webhistory/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.webhistory
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- frontend/src/lib/screens/all_screen.dart
+++
@@ -1,4 +0,0 @@
-export 'details_screen.dart';
-export 'insert_screen.dart';
-export 'login_screen.dart';
-export 'main_screen.dart';
