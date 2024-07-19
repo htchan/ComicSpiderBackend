c870e508b055870970fd92e726635543e7d6fa05
htchan <htchanbc@connect.ust.hk>
2024-07-19 15:49:19 +0800

    feat: add general info

---
+++ cmd/api/swagger.go
@@ -0,0 +1,7 @@
+package main
+
+// @title Web History Backend
+// @version 1.0
+// @description This is a service checking last update of specific websites
+
+//go:generate swag init -d . -g swagger.go -o ../../docs -ot go,json
