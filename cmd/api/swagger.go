0eb132883465397006899f0d15fd2925cf2b5ad6
htchan <htchanbc@connect.ust.hk>
2024-07-19 17:45:27 +0800

    feat: add router and upload docs

--- cmd/api/swagger.go
+++ cmd/api/swagger.go
@@ -1,7 +1,8 @@
 package main
 
-// @title Web History Backend
-// @version 1.0
-// @description This is a service checking last update of specific websites
+//	@title			Web History Backend
+//	@version		1.0
+//	@description	This is a service checking last update of specific websites
 
-//go:generate swag init -d . -g swagger.go -o ../../docs -ot go,json
+//go:generate swag fmt -d .,../../internal
+//go:generate swag init -d ../../ -g cmd/api/swagger.go -o ../../docs -ot go,json
