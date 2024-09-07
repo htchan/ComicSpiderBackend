c529384b98e5781a64b385f25dbb64fe8672414d
htchan <htchanbc@connect.ust.hk>
2024-09-07 13:39:11 +0800

    chore: remove the not useful pprof because no history data

--- internal/router/website/router.go
+++ internal/router/website/router.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"net/http/pprof"
 	"os"
 
 	"github.com/go-chi/chi/v5"
@@ -67,12 +66,4 @@ func AddRoutes(router chi.Router, r repository.Repostory, conf *config.APIConfig
 	})
 
 	router.Get("/docs/swagger/*", httpSwagger.Handler())
-
-	router.Route("/debug/pprof", func(router chi.Router) {
-		router.Get("/", pprof.Index)
-		router.Get("/cmdline", pprof.Cmdline)
-		router.Get("/profile", pprof.Profile)
-		router.Get("/symbol", pprof.Symbol)
-		router.Get("/trace", pprof.Trace)
-	})
 }
