9b722e79d522433b7ad23614fc7b0a491e790081
htchan <htchanbc@connect.ust.hk>
2024-09-21 18:08:46 +0800

    chore: remove executor and jobs after adapt to goworker

--- internal/jobs/errors.go
+++
@@ -1,8 +0,0 @@
-package jobs
-
-import "errors"
-
-var (
-	ErrInvalidParams    = errors.New("invalid params")
-	ErrSchedulerStopped = errors.New("scheduler stopped")
-)
--- internal/jobs/interface.go
+++
@@ -1,9 +0,0 @@
-package jobs
-
-import "github.com/htchan/WebHistory/internal/executor"
-
-type Scheduler interface {
-	Start()
-	Stop() error
-	Publisher() executor.JobTrigger
-}
