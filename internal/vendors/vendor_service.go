7426bd9e26f7613f3e2221991416cd49dd0ac473
htchan <jacky19980619@gmail.com>
2025-09-17 21:47:51 +0800

    chore: add middleware to raise error when status code not between 200 to 299

--- internal/vendors/vendor_service.go
+++ internal/vendors/vendor_service.go
@@ -3,8 +3,10 @@ package vendors
 import (
 	"context"
 	"fmt"
+	"net/http"
 
 	"github.com/htchan/WebHistory/internal/model"
+	"github.com/htchan/goclient"
 )
 
 var ErrInvalidStatusCode = fmt.Errorf("invalid status code")
@@ -16,4 +18,15 @@ type VendorService interface {
 	Update(context.Context, *model.Website) error
 	Name() string
 	// Download(context.Context, *model.Website) error
+}
+
+func RaiseStatusCodeErrorMiddleware(f goclient.Requester) goclient.Requester {
+	return func(req *http.Request) (*http.Response, error) {
+		resp, err := f(req)
+		if err != nil || (resp.StatusCode >= 200 && resp.StatusCode < 300) {
+			return resp, err
+		}
+
+		return resp, fmt.Errorf("fetch website failed: %w (%d)", ErrInvalidStatusCode, resp.StatusCode)
+	}
 }
